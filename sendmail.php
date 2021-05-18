0457d9bd6f96188c0a4941aeb57053429897035e
Kirill Romanenko <brs.group.com@gmail.com>
2021-05-18 21:41:50 +0300

    added sending mail

---
+++ sendmail.php
@@ -0,0 +1,36 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'phpmailer/src/Exception.php';
+    require 'phpmailer/src/PHPMailer.php';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+>
