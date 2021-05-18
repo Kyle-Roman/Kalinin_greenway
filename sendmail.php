c5132edd2781cd47e0a5886c5f2e7850b9cd4641
Kirill Romanenko <brs.group.com@gmail.com>
2021-05-18 22:03:57 +0300

    added phpmailer

--- sendmail.php
+++ sendmail.php
@@ -5,6 +5,45 @@
     require 'phpmailer/src/Exception.php';
     require 'phpmailer/src/PHPMailer.php';
 
+    $mail = new PHPMailer(true);
+    $mail->CharSet  = 'UTF-8';
+    $mail->setLanguage('ru', 'phpmailer/language/');
+    $mail->IsHTML(true);
+
+    // send mail from...
+    $mail->setFrom('info@brsgroup.lv', 'Отправитель');
+    // sending to...
+    $mail->addAddress('mihails.kalinins@inbox.lv');
+    // letters theme
+    $mail->Subject = 'Новый потенциальный партнер';
+
+    // letters body
+    $body = '<h1>Hawdy partner!</h1>';
+
+    if(trim(!empty($_POST['user-name']))){
+        $body.='<p><strong>Имя:</strong> '.$_POST['user-name'].'</p>';
+    }
+    if(trim(!empty($_POST['user-phone']))){
+        $body.='<p><strong>Телефон:</strong> '.$_POST['user-phone'].'</p>';
+    }
+    if(trim(!empty($_POST['user-message']))){
+        $body.='<p><strong>Сообщение:</strong> '.$_POST['user-message'].'</p>';
+    }
+
+
+    // sending
+
+    if(!$mail->send()){
+        $message = 'Ошибка';
+    } else {
+        $message = 'Данные отправлены!';
+    }
+
+    $response = ['message' => $message];
+
+    header('Content-type: application/json');
+    echo json_encode($response);
+?>
 
 
 
